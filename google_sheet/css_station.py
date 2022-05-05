f6834afedd02fe89e7993d57a68e9c2cf99ec046
brodsa <brodinova@gmail.com>
2022-05-05 23:23:36 +0200

    display next

--- google_sheet/css_station.py
+++ google_sheet/css_station.py
@@ -1,10 +1,10 @@
-from data_google_sheet import df_station, max_station
+from data_google_sheet import df_station
 
-with open("style_station.css", "w") as f_station:
+with open("./css/style_station.css", "w") as f_station:
     f_station.write(
         f"""
         .station-a::after {{
-            content: \"{df_station["Station A"]}\"
+            content: \"{df_station.loc["A","w_teams_done"]}\"
         }}
         
 
--- google_sheet/css_team.py
+++ google_sheet/css_team.py
@@ -1,10 +1,10 @@
 from data_google_sheet import df_team
 
-with open("style_teams.css", "w") as f_team:
+with open("./css/style_teams.css", "w") as f_team:
     f_team.write(
          f"""
         .team-a::after {{
-            content: \" {df_team.loc["Cevedale", "Score"]} \"
+            content: \" {df_team.loc["CE","w_score"]} \"
         }}
         
 
