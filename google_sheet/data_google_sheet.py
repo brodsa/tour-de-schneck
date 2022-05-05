1451223889b654fda421a163af946cea1d59912f
brodsa <brodinova@gmail.com>
2022-05-05 17:08:23 +0200

    color and short name of teams added

--- google_sheet/data_google_sheet.py
+++ google_sheet/data_google_sheet.py
@@ -13,15 +13,23 @@ worksheet = sh.worksheet("Ergebnisse")
 list_of_dicts = worksheet.get_all_records()
 df_original = pd.DataFrame(list_of_dicts)
 
+worksheet_color = sh.worksheet("Stationsübersicht")
+color_list = worksheet_color.get_all_records()
+color_df = pd.DataFrame(color_list)
 
 # Data summary for teams and stations
 df = df_original[["Team", "Station", "Score", "Originelle_Zusatzpunkte"]].drop_duplicates().dropna()
+df_color = color_df[["Gruppennamen","short","color"]].dropna().iloc[0:25]
+df_color["Gruppe"] = df_color["Gruppennamen"]
 
 # teams summary
-df_team = df.groupby("Team").sum()  
+df_team = df.groupby("Team").sum()
 df_team.sort_values(by=['Score'], inplace=True, ascending=False)
 df_team['Order'] = list(range(df_team.shape[0]))
-df_team_station = df['Team'].value_counts() 
+df_team["Gruppe"] = df_team.index
+df_team = pd.merge(df_team,df_color,on=["Gruppe"])
+
+df_team_station = df['Team'].value_counts()
 max_team = len(df['Team'].unique())
 max_score= 10 * 13
 
@@ -31,10 +39,11 @@ max_station = 13
 
 
 # Example to display
-example_score = df_team.loc["Cevedale", "Score"]
+
+example_score = int(df_team[df_team["Gruppe"] == "Cevedale"]["Score"])
 example_team_station = df_team_station["Cevedale"]
 example_station = df_station["Station A"]
-example_order = df_team.loc["Cevedale", "Order"]
+example_order = int(df_team[df_team["Gruppe"] == "Cevedale"]["Order"])
 print(f"Score: {example_score}/{max_score}")
 print(f"Stationen: {example_team_station}/{max_station}")
 print(f"Teamsreihnfolge: {example_order}")
