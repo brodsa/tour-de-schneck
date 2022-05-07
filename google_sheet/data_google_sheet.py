f51c3951a2e8f3db4ba66aea59e8ad240f5ecfc2
brodsa <brodinova@gmail.com>
2022-05-08 00:08:08 +0200

    bonus points done

--- google_sheet/data_google_sheet.py
+++ google_sheet/data_google_sheet.py
@@ -80,7 +80,6 @@ print("Abbkürzung: {}".format(df_team.loc["CE","short"]))
 df_team["Teamname"] = df_team["short"]
 # Visualization
 df_team.sort_values(by=["Score"],inplace=True,ascending=False)
-
 colors = df_team["color"]
 sns.set_palette(sns.color_palette(colors))
 
@@ -90,4 +89,18 @@ plt.xlabel('Team', fontsize=20)
 plt.ylabel('Score', fontsize=20)
 plt.xticks(fontsize= 18)
 plt.yticks(fontsize= 18)
-plt.savefig("./img/300.png" , dpi = 200)
+plt.savefig("./img/Score.png" , dpi = 200)
+
+df_team.sort_values(by=["Originelle_Zusatzpunkte"],inplace=True,ascending=False)
+colors = df_team["color"]
+sns.set_palette(sns.color_palette(colors))
+
+plt.figure(figsize=(20,10), dpi=200)
+sns.barplot(y = 'Originelle_Zusatzpunkte', x="Teamname", data = df_team)
+plt.xlabel('Team', fontsize=20)
+plt.ylabel('Bonuspunkte', fontsize=20)
+plt.xticks(fontsize= 18)
+plt.yticks(fontsize= 18)
+plt.savefig("./img/Bonus.png" , dpi = 200)
+
+
