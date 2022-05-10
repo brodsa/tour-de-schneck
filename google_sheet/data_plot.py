b7d40aa5bcbdceecc9b728ea9ded11fe689692ff
brodsa <brodinova@gmail.com>
2022-05-10 14:14:39 +0200

    update

--- google_sheet/data_plot.py
+++ google_sheet/data_plot.py
@@ -36,23 +36,32 @@ from data_google_sheet import df_team
 
 df_plot = df_team[["Originelle_Zusatzpunkte","w_points"]]
 df_plot.reset_index(inplace=True)
-df_plot.columns = [ "Team","Bonus","Score"]
-print(df_plot)
+df_plot.columns = [ "Team","Bonus","Score",]
+#print(df_plot)
+
+# plt.figure(figsize=(20,10), dpi=200)
+# bar_plot1 = sns.barplot(x='Team', y='Score', data=df_plot, label="Score", color="gray")
+# bar_plot2 = sns.barplot(x='Team', y='Bonus', data=df_plot, label="Bonus", color="#AA2C2E")
+# plt.legend(ncol=2, loc="upper right", frameon=True,fontsize=20)
+# plt.xticks(rotation=0)
+# plt.xticks(fontsize= 18)
+# plt.yticks(fontsize= 18)
+# plt.xlabel('Team', fontsize=20)
+# plt.ylabel('Punkte', fontsize=20)
+# plt.savefig("./img/Gesamt.png" , dpi = 200)
+
 
 plt.figure(figsize=(20,10), dpi=200)
-bar_plot1 = sns.barplot(x='Team', y='Score', data=df_plot, label="Score", color="gray")
-bar_plot2 = sns.barplot(x='Team', y='Bonus', data=df_plot, label="Bonus", color="#AA2C2E")
+colors = ["#AA2C2E","gray"]
+sns.set_palette(sns.color_palette(colors))
+df_pl = pd.melt(df_plot,value_vars=["Score","Bonus"],id_vars="Team")
+df_pl.columns = [ "Teams","Punkte","Anzahl"]
+print(df_pl)
+sns.barplot(x ="Teams", y = 'Anzahl', data = df_pl, hue = "Punkte")
 plt.legend(ncol=2, loc="upper right", frameon=True,fontsize=20)
-plt.xticks(rotation=0)
 plt.xticks(fontsize= 18)
 plt.yticks(fontsize= 18)
 plt.xlabel('Team', fontsize=20)
 plt.ylabel('Punkte', fontsize=20)
-plt.savefig("./img/Gesamt.png" , dpi = 200)
-
-
-
-# df_pl = pd.melt(df_plot,value_vars=["Score","Bonus"],id_vars="short")
-# df_pl.columns = [ "Teams","Punkte","Anzahl"]
-# print(df_pl)
-# sns.barplot(x ="Teams", y = 'Anzahl', data = df_pl, hue = "Punkte")
+plt.savefig("./img/Gesamt_unstack.png" , dpi = 200)
+# plt.show()
